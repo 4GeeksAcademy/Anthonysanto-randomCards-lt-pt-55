4e7f4649a31430022f01974c443b4a307d0d45b8
Anthony González <asgtoribio@gmail.com>
2025-07-03 01:56:55 +0000

    finalizacion de codigo

--- src/app.js
+++ src/app.js
@@ -2,7 +2,24 @@
 window.onload = function() {
   //write your code here
  
+  const allIcons = ["♦","♥", "♠", "♣"];
+  const allValues =['A','1','2','3','4','5','6','7','8','9','10','J','K','Q']
+
+  let randomValues = allValues[Math.floor(Math.random() * allValues.length)] ;
   
-  document.getElementById('getNumber').textContent=Math.floor(Math.random()*4)+1;
-  document.getElementById('getIcon').textContent=Math.floor(Math.random()*12)+1;
+  let randomIcon = allIcons[Math.floor(Math.random() * allIcons.length)];
+
+  let color; 
+  if (randomIcon == "♦" || randomIcon == "♥") {
+  color= 'red';
+} else {
+  color= 'black';}
+  
+  document.getElementById('getNumber').textContent= randomValues;
+  document.getElementById('getIconTop').textContent= randomIcon;
+  document.getElementById('getIconBottom').textContent= randomIcon;
+  document.getElementById('getIconTop').style.color= color;
+  document.getElementById('getIconBottom').style.color= color;
+
 };
+
